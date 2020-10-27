93a4ceb693386f8bc6604eeb0fc95dc7024ead7b
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2020-10-28 04:56:45 +0600

    Developed ATTACK functionality

---
+++ src/Monpoke.js
@@ -0,0 +1,237 @@
+const prompt = require("prompt-async");
+const validateTeamCreationDataTypes = require("../utils/validator");
+
+const Monpoke = {};
+
+Monpoke.teams = [];
+Monpoke.currentTeam = [];
+Monpoke.started = false;
+Monpoke.playerTurn = 0;
+Monpoke.winner = -1;
+
+// Welcome message to players
+Monpoke.welcome = () => {
+  const welcomeMsg = `Welcome to Monpoke, a turn based battle system - where two teams face off against each other with their respective Monpoke! The games rules are as below: \n* The game consists of 2 teams, each team has a variable number of Monpoké. \n* Each Monpoké has 2 attributes, HitPoints (HP) and Attack Power (AP). \n* The 2 teams engage in a simple turn-based battle. \n* A team’s turn can be either choosing a Monpoké OR attacking with their currently \nchosen Monpoké. \n* Attacking Monpoké depletes the enemy Monpoké HP for the value of their AP. \n* A Monpoké is defeated when its HP is less than or equal to 0. \n* The game ends when all of a team’s Monpoké have been defeated. \nTo play the game, you have the following commands available to you: \n* CREATE <team-id> <monpoké-id> <hp> <attack> -> to create a team \n* ICHOOSEYOU <monpoké-id> -> to select current Monpoke \n* ATTACK -> attack opponent using your selected Monpoke \nGood luck!`;
+
+  return welcomeMsg;
+};
+
+// Function to allow user to send command to game
+Monpoke.sendCommand = async () => {
+  prompt.message = "";
+  prompt.delimiter = "";
+  prompt.description = "";
+
+  prompt.start();
+
+  const { COMMAND } = await prompt.get(["COMMAND"]);
+  prompt.stop();
+  const userCommand = COMMAND.split(" ");
+
+  switch (userCommand[0]) {
+    case "CREATE":
+      return "Team creation";
+    case "ICHOOSEYOU":
+      return "Monpoke chosen";
+    case "ATTACK":
+      return "Attacking opponent";
+    default:
+      throw new SyntaxError();
+  }
+};
+
+Monpoke.createTeam = (teamName, monpokeID, hp, ap) => {
+  if (Monpoke.started) {
+    throw new Error("Cannot run CREATE team after game has started!");
+  }
+
+  if (hp < 1) {
+    throw new RangeError("Monpoke must have HP of 1 or higher");
+  }
+
+  if (ap < 1) {
+    throw new RangeError("Monpoke must have AP of 1 or higher");
+  }
+
+  if (validateTeamCreationDataTypes(teamName, monpokeID, hp, ap)) {
+    const newMonpoke = {
+      monpokeID: monpokeID,
+      hp: hp,
+      ap: ap,
+    };
+
+    const team = {};
+
+    if (!Monpoke.teams.length) {
+      team.name = teamName;
+      team.monpoke = [newMonpoke];
+
+      Monpoke.teams.push(team);
+      Monpoke.initCurrentTeam(teamName);
+
+      return `${monpokeID} has been assigned to team ${teamName}`;
+    } else if (Monpoke.teams.length < 2) {
+      for (teamMember of Monpoke.teams) {
+        if (teamMember.name === teamName) {
+          for (monpokeMember of teamMember.monpoke) {
+            if (monpokeMember.monpokeID === monpokeID) {
+              throw new Error("Monpoke already assigned to team!");
+            }
+          }
+          teamMember.monpoke.push(newMonpoke);
+          return `${monpokeID} has been assigned to team ${teamName}`;
+        }
+      }
+
+      team.name = teamName;
+      team.monpoke = [newMonpoke];
+      Monpoke.teams.push(team);
+      Monpoke.initCurrentTeam(teamName);
+      return `${monpokeID} has been assigned to team ${teamName}`;
+    }
+
+    let partOfTeam = false;
+
+    if (Monpoke.teams.length === 2) {
+      for (teamMember of Monpoke.teams) {
+        if (teamMember.name === teamName) {
+          for (monpokeMember of teamMember.monpoke) {
+            if (monpokeMember.monpokeID === monpokeID) {
+              throw new Error("Monpoke already assigned to team!");
+            }
+          }
+          partOfTeam = true;
+          teamMember.monpoke.push(newMonpoke);
+          return `${monpokeID} has been assigned to team ${teamName}`;
+        } else {
+          partOfTeam = false;
+        }
+      }
+
+      if (partOfTeam) {
+        team.name = teamName;
+        team.monpoke = [newMonpoke];
+        Monpoke.teams.push(team);
+        Monpoke.initCurrentTeam(teamName);
+        return `${monpokeID} has been assigned to team ${teamName}`;
+      } else {
+        return "Cannot have more than 2 teams";
+      }
+    }
+  }
+};
+
+Monpoke.initCurrentTeam = (teamName) => {
+  const team = {
+    name: teamName,
+    monpoke: {},
+  };
+
+  Monpoke.currentTeam.push(team);
+};
+
+Monpoke.chooseMonpoke = (monpokeID) => {
+  if (Monpoke.currentTeam.length < 2) {
+    throw new Error("Total number of teams has to be 2!");
+  }
+
+  let monpokeStats,
+    found = false;
+
+  const team = Monpoke.teams[Monpoke.playerTurn];
+  for (monpoke of team.monpoke) {
+    // console.log(monpoke);
+    if (monpoke.monpokeID === monpokeID) {
+      monpokeStats = monpoke;
+      found = true;
+    }
+  }
+  if (!found) throw new Error("Monpoke does not exist on your team!");
+
+  Monpoke.currentTeam[Monpoke.playerTurn].monpoke = monpokeStats;
+
+  Monpoke.started = true;
+
+  //   Iterating through currentTeam to check if Monpoke has been chosen
+  for (teamMember of Monpoke.currentTeam) {
+    if (Object.keys(teamMember.monpoke).length < 1) {
+      Monpoke.started = false;
+    }
+  }
+
+  //   Switching player turn
+  Monpoke.playerTurn = Monpoke.playerTurn === 0 ? 1 : 0;
+
+  return `${monpokeStats.monpokeID} has entered the battle!`;
+};
+
+Monpoke.attack = () => {
+  if (!Monpoke.started) {
+    throw new Error("You have to choose a Monpoke before attacking!");
+  }
+
+  let msg;
+  const currentHP = Monpoke.currentTeam[Monpoke.playerTurn].monpoke.hp;
+  const currentMonpoke =
+    Monpoke.currentTeam[Monpoke.playerTurn].monpoke.monpokeID;
+
+  if (currentHP < 1) {
+    msg = `${currentMonpoke} has been defeated! Please choose new Monpoke`;
+    return msg;
+  }
+
+  const attackerAP = Monpoke.currentTeam[Monpoke.playerTurn].monpoke.ap;
+  const opponentIndex = Monpoke.playerTurn === 0 ? 1 : 0;
+  const opponentMonpoke = Monpoke.currentTeam[opponentIndex].monpoke.monpokeID;
+  //   Reducing opponent's HP
+  Monpoke.currentTeam[opponentIndex].monpoke.hp -= attackerAP;
+
+  for (monpokeMember of Monpoke.teams[opponentIndex].monpoke) {
+    if (monpokeMember.name === opponentMonpoke) {
+      monpokeMember.hp -= attackerAP;
+    }
+  }
+  //   Monpoke.teams[opponentIndex].monpoke.hp -= attackerAP;
+
+  msg = `${currentMonpoke} attacked ${opponentMonpoke} for ${attackerAP} damage!`;
+
+  if (Monpoke.currentTeam[opponentIndex].monpoke.hp < 1) {
+    msg = `${opponentMonpoke} has been defeated!`;
+  }
+
+  const defeated = Monpoke.isDefeated();
+
+  if (defeated !== -1) {
+    msg = `${Monpoke.currentTeam[defeated === 0 ? 1 : 1].name} is the winner!`;
+    Monpoke.winner = defeated === 0 ? 1 : 1;
+  }
+
+  // Switching player turn
+  Monpoke.playerTurn = opponentIndex;
+
+  return msg;
+};
+
+Monpoke.isDefeated = () => {
+  let monpokeCount = [0, 0];
+  let defeatedCount = [0, 0];
+
+  for (let i = 0; i < Monpoke.teams.length; i++) {
+    for (j = 0; j < Monpoke.teams[i].monpoke.length; j++) {
+      monpokeCount[i]++;
+      if (Monpoke.teams[i].monpoke[j].hp < 1) {
+        defeatedCount[i]++;
+      }
+    }
+  }
+
+  if (monpokeCount[0] === defeatedCount[0]) {
+    return 0;
+  } else if (monpokeCount[1] === defeatedCount[1]) {
+    return 1;
+  } else {
+    return -1;
+  }
+};
+
+module.exports = Monpoke;
--- tests/monpoke_test.js
+++ tests/monpoke_test.js
@@ -6,7 +6,7 @@ chai.use(chaiAsPromised);
 const assert = chai.assert;
 const expect = chai.expect;
 
-const Monpoke = require("../src/index");
+const Monpoke = require("../src/Monpoke");
 
 describe("player starts the game", () => {
   it("should display welcome message", () => {
@@ -53,6 +53,7 @@ describe("player enters commands", () => {
     beforeEach(() => {
       Monpoke.teams = [];
       Monpoke.currentTeam = [];
+      Monpoke.started = false;
     });
 
     it("should create team with passed arguments - red team", () => {
@@ -120,7 +121,6 @@ describe("player enters commands", () => {
       Monpoke.createTeam("orange", "watermillion", 5, 6);
 
       const result = Monpoke.teams.length;
-
       assert.isAtMost(result, expectedNumTeams);
     });
 
@@ -226,13 +226,49 @@ describe("player enters commands", () => {
       assert.deepEqual(Monpoke.currentTeam, expected);
     });
 
-    it('should throw error ')
+    it("should throw error when trying to run CREATE after current team has been initialized", () => {
+      Monpoke.createTeam("red", "reekachu", 7, 5);
+      Monpoke.createTeam("blue", "smorelax", 10, 3);
+
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
+
+      assert.throws(
+        () => {
+          Monpoke.createTeam("red", "bulbee", 7, 6);
+        },
+        Error,
+        "Cannot run CREATE team after game has started!"
+      );
+    });
+
+    it("should throw error when Monpoke has hp of less than 1", () => {
+      assert.throws(
+        () => {
+          Monpoke.createTeam("red", "smorelax", -2, 5);
+        },
+        RangeError,
+        "Monpoke must have HP of 1 or higher"
+      );
+    });
+
+    it("should throw error when Monpoke has ap of less than 1", () => {
+      assert.throws(
+        () => {
+          Monpoke.createTeam("red", "smorelax", 2, -5);
+        },
+        RangeError,
+        "Monpoke must have AP of 1 or higher"
+      );
+    });
   });
 
   describe("player runs ICHOOSEYOU command", () => {
     beforeEach(() => {
       Monpoke.teams = [];
       Monpoke.currentTeam = [];
+      Monpoke.started = false;
+      Monpoke.playerTurn = 0;
     });
 
     it("should throw error if total number of teams is not 2", () => {
@@ -259,7 +295,7 @@ describe("player enters commands", () => {
       Monpoke.createTeam("red", "bulbee", 7, 6);
       Monpoke.createTeam("blue", "smorelax", 10, 3);
 
-      Monpoke.chooseMonpoke("red", "reekachu");
+      Monpoke.chooseMonpoke("reekachu");
 
       assert.deepNestedInclude(Monpoke.currentTeam, expected);
     });
@@ -288,21 +324,152 @@ describe("player enters commands", () => {
       Monpoke.createTeam("red", "bulbee", 7, 6);
       Monpoke.createTeam("blue", "smorelax", 10, 3);
 
-      Monpoke.chooseMonpoke("red", "reekachu");
-      Monpoke.chooseMonpoke("blue", "smorelax");
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
 
       assert.deepStrictEqual(Monpoke.currentTeam, expected);
     });
 
-    it('should throw error when choosing non-existent monpoke', () => {
+    it("should throw error when choosing non-existent monpoke", () => {
       Monpoke.createTeam("red", "reekachu", 7, 5);
       Monpoke.createTeam("red", "bulbee", 7, 6);
       Monpoke.createTeam("blue", "smorelax", 10, 3);
 
-      assert.throws(() => {
-        Monpoke.chooseMonpoke("blue", "reekachu")
-      }, Error, 'Monpoke does not exist on your team!')
-    })
+      assert.throws(
+        () => {
+          Monpoke.chooseMonpoke("smorelax");
+        },
+        Error,
+        "Monpoke does not exist on your team!"
+      );
+    });
 
+    it("should produce output when Monpoke is chosen", () => {
+      const expected = "reekachu has entered the battle!";
+
+      Monpoke.createTeam("red", "reekachu", 7, 5);
+      Monpoke.createTeam("red", "bulbee", 7, 6);
+      Monpoke.createTeam("blue", "smorelax", 10, 3);
+
+      const result = Monpoke.chooseMonpoke("reekachu");
+
+      assert.strictEqual(result, expected);
+    });
+  });
+
+  describe("player runs ATTACK command", () => {
+    beforeEach(() => {
+      Monpoke.teams = [];
+      Monpoke.currentTeam = [];
+      Monpoke.started = false;
+      Monpoke.playerTurn = 0;
+
+      Monpoke.createTeam("red", "reekachu", 7, 5);
+      Monpoke.createTeam("blue", "smorelax", 10, 3);
+      Monpoke.createTeam("blue", "graxxcutter", 7, 6);
+    });
+
+    it("should decrease opponent's HP by attacker's AP", () => {
+      const expected = {
+        name: "blue",
+        monpoke: {
+          monpokeID: "smorelax",
+          hp: 5,
+          ap: 3,
+        },
+      };
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
+
+      Monpoke.attack();
+
+      const result = Monpoke.currentTeam[1];
+
+      assert.deepNestedInclude(result, expected);
+    });
+
+    it("should decrease Player 1's HP by Player 2's AP - when Player 2 attacks", () => {
+      const expected = {
+        name: "red",
+        monpoke: {
+          monpokeID: "reekachu",
+          hp: 4,
+          ap: 5,
+        },
+      };
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
+
+      Monpoke.attack();
+      Monpoke.attack();
+
+      const result = Monpoke.currentTeam[0];
+
+      assert.deepNestedInclude(result, expected);
+    });
+
+    it("should throw error when attacking without choosing Monpoke", () => {
+      assert.throws(
+        () => {
+          Monpoke.attack();
+        },
+        Error,
+        "You have to choose a Monpoke before attacking!"
+      );
+    });
+
+    it("should produce output to let player know of damage dealt to opponent", () => {
+      const expected = "reekachu attacked smorelax for 5 damage!";
+
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
+
+      const result = Monpoke.attack();
+
+      assert.strictEqual(result, expected);
+    });
+
+    it("should defeat enemy Monpoke if HP is less than 1", () => {
+      const expected = "smorelax has been defeated!";
+
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
+
+      Monpoke.attack();
+      Monpoke.attack();
+      const result = Monpoke.attack();
+
+      assert.strictEqual(result, expected);
+    });
+
+    it("should output error when Player tries to attack with defeated Monpoke", () => {
+      const expected = "smorelax has been defeated! Please choose new Monpoke";
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
+
+      Monpoke.attack();
+      Monpoke.attack();
+      Monpoke.attack();
+
+      const result = Monpoke.attack();
+
+      assert.strictEqual(result, expected);
+    });
+
+    it("should declare winner when all Monpoke on team have 0 HP", () => {
+      const expected = "blue is the winner!";
+
+      Monpoke.chooseMonpoke("reekachu");
+      Monpoke.chooseMonpoke("smorelax");
+
+      Monpoke.attack();
+      Monpoke.attack();
+      Monpoke.attack();
+      Monpoke.chooseMonpoke("graxxcutter");
+      Monpoke.attack();
+      const result = Monpoke.attack();
+
+      assert.strictEqual(result, expected);
+    });
   });
 });
